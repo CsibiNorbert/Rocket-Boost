9cb32aa0e5fbe94fb5ec17213ce8ea25d0f18b68
Norbert Csibi <cibi_norbert@yahoo.com>
2021-07-03 18:49:09 +0100

    Added rocket boost project
    Added rocket boost project

---
+++ Project Boost/Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float rocketBoostSpeed = 1000f;
+    [SerializeField]
+    private float rocketRotationSpeed = 50f;
+
+    private Rigidbody _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessBoost();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rocketRotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rocketRotationSpeed);
+        }
+    }
+
+    private void ApplyRotation(float rotationFrame)
+    {
+        _rigidBody.freezeRotation = true; // freezing rotation so that we can manually rotate
+        transform.Rotate(Vector3.right * rotationFrame * Time.deltaTime);
+        _rigidBody.freezeRotation = false; // unfreezing rotation so that the world physics can take over
+    }
+
+    private void ProcessBoost()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // We add the relative force because if we rotate, we need to point into the direction in which is rotated
+            _rigidBody.AddRelativeForce(Vector3.up * rocketBoostSpeed * Time.deltaTime);
+        }
+    }
+}
